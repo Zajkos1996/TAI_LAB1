c64029efb122139df107c6bc5ed13655396cb55e
Zajkos1996 <zajkos016@gmail.com>
2019-03-08 16:18:05 +0100

    Dodawanie stopki oraz tworzenie skryptu do strony :)

---
+++ navbar.js
@@ -0,0 +1,6 @@
+let nav = '<nav><a  href="index.html"> HOME </a>' +
+    '<a  href="#">BLOG</a>' +
+    '<a  href="#">QUIZ</a>' +
+    '<a  href="contact.html" > KONTAKT </a> </nav>';
+let elem4 = document.querySelector(".navbar");
+elem4.innerHTML= nav;
