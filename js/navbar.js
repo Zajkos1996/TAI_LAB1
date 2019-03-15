a5cb375839b053a174586dbb8985e948974ec4ae
Zajkos1996 <zajkos016@gmail.com>
2019-03-15 16:01:21 +0100

    Dodanie przycisku: "Do góry "

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,6 @@
-let nav = '<nav><a  href="../index.html"> HOME </a>' +
+let nav = '<nav><a  href="index.html"> HOME </a>' +
     '<a  href="#">BLOG</a>' +
     '<a  href="#">QUIZ</a>' +
-    '<a  href="../contact.html" > KONTAKT </a> </nav>';
+    '<a  href="contact.html" > KONTAKT </a> </nav>';
 let elem4 = document.querySelector(".navbar");
 elem4.innerHTML= nav;
