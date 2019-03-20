688e44c5f993d726cb5b21d07492ba80c149d507
Zajkos1996 <zajkos016@gmail.com>
2019-03-20 14:55:26 +0100

    Quiz

--- js/script.js
+++ js/script.js
@@ -1,105 +1,98 @@
-let next = document.querySelector('.next');
-let previous = document.querySelector('.previous');
-let question = document.querySelector('.question');
-let questionNumber = document.querySelector('.questionNumber');
-let answers = document.querySelectorAll('.list-group-item');
-let pointsElem = document.querySelector('.score');
-let restart = document.querySelector('.restart');
-
-let index = 0;
-let points = 0;
-
-let list = document.querySelector('.list');
-let results = document.querySelector('.results');
-let userScorePoint = document.querySelector('.userScorePoint');
-let averageScore = document.querySelector('.average');
-
-let preQuestions = [];
 fetch('https://quiztai.herokuapp.com/api/quiz')
     .then(resp => resp.json())
     .then(resp => {
         preQuestions = resp;
 
-        setQuestion(index);
-        activateAnswers();
+        setQuestion(0);
+        previous.addEventListener('click', function () {
+            if (index > 0) {
+                index--;
+                setQuestion(index);
+                activateAnswers();
+            }
+        });
 
         next.addEventListener('click', function () {
-
-            if(index < preQuestions.length - 1){
-                index++;
-                setQuestion(index);
-                questionNumber.innerHTML = index + 1;
-            } else {
+            index++;
+            if (index >= preQuestions.length) {
                 list.style.display = 'none';
                 results.style.display = 'block';
                 userScorePoint.innerHTML = points;
-                saveResult();
-            }
-            activateAnswers();
 
-        });
+                previousScore = JSON.parse(localStorage.getItem("previousScore"));
+                games = JSON.parse(localStorage.getItem("games"));
+                if(previousScore === null && games === null){
+                    avg = points;
+                    games = 0;
+                }else{
+                    avg = ((previousScore * games) + points) / (games + 1);
+                }
 
-        previous.addEventListener('click', function () {
-            if(index > 0){
-                index--;
+                average.innerHTML = avg;
+                localStorage.setItem("previousScore", JSON.stringify(avg));
+                localStorage.setItem("games", JSON.stringify(games + 1));
+
+            } else {
                 setQuestion(index);
-                questionNumber.innerHTML = index + 1;
+                activateAnswers();
             }
-            activateAnswers();
         });
 
-        restart.addEventListener("click", () => {
-            list.style.display = 'block';
-            results.style.display = 'none';
-            userScorePoint.style.display = 'block';
+        restart.addEventListener('click', function (event) {
+            event.preventDefault();
+
             index = 0;
             points = 0;
-            pointsElem.innerText = points;
+            let userScorePoint = document.querySelector('.score');
+            userScorePoint.innerHTML = points;
             setQuestion(index);
+            activateAnswers();
+            list.style.display = 'block';
+            results.style.display = 'none';
         });
 
+        function setQuestion(index) {
+            clearClass();
+            number.innerHTML = (index + 1);
+            question.innerHTML = preQuestions[index].question;
+
+            answers[0].innerHTML = preQuestions[index].answers[0];
+            answers[1].innerHTML = preQuestions[index].answers[1];
+            answers[2].innerHTML = preQuestions[index].answers[2];
+            answers[3].innerHTML = preQuestions[index].answers[3];
+
+            if (preQuestions[index].answers.length === 2) {
+                answers[2].style.display = 'none';
+                answers[3].style.display = 'none';
+            } else {
+                answers[2].style.display = 'block';
+                answers[3].style.display = 'block';
+            }
+
+        }
     });
 
+let next = document.querySelector('.next');
+let previous = document.querySelector('.previous');
 
+let number = document.querySelector('.number');
+let question = document.querySelector('.question');
+let answers = document.querySelectorAll('.list-group-item');
 
-function setQuestion(index) {
-    clearClass();
-    question.innerHTML = preQuestions[index].question;
-    questionNumber.innerHTML = index + 1;
-    answers[0].innerHTML = preQuestions[index].answers[0];
-    answers[1].innerHTML = preQuestions[index].answers[1];
-    answers[2].innerHTML = preQuestions[index].answers[2];
-    answers[3].innerHTML = preQuestions[index].answers[3];
+let pointsElem = document.querySelector('.score');
+let restart = document.querySelector('.restart');
 
-    if (preQuestions[index].answers.length === 2) {
-        answers[2].style.display = 'none';
-        answers[3].style.display = 'none';
-    } else {
-        answers[2].style.display = 'block';
-        answers[3].style.display = 'block';
-    }
-}
+let list = document.querySelector('.list');
+let results = document.querySelector('.results');
+let userScorePoint = document.querySelector('.userScorePoint');
 
+let average = document.querySelector('.average');
 
+let index = 0;
+let points = 0;
 
-function activateAnswers() {
-    for (let i = 0; i < answers.length; i++) {
-        answers[i].addEventListener('click', doAction);
-    }
-}
-
-
-function doAction(event) {
-
-    if (event.target.innerHTML === preQuestions[index].correct_answer) {
-        points++;
-        pointsElem.innerText = points;
-        markCorrect(event.target);
-    }
-    else {
-        markInCorrect(event.target);
-    }
-    disableAnswers();
+for (let i = 0; i < answers.length; i++) {
+    answers[i].addEventListener('click', doAction);
 }
 
 function markCorrect(elem) {
@@ -116,32 +109,28 @@ function disableAnswers() {
     }
 }
 
+function doAction(event) {
+    if (event.target.innerHTML === preQuestions[index].correct_answer) {
+        points++;
+        pointsElem.innerText = points;
+        markCorrect(event.target);
+
+    } else {
+        markInCorrect(event.target);
+    }
+    disableAnswers();
+}
+
+function activateAnswers() {
+    for (let i = 0; i < answers.length; i++) {
+        answers[i].addEventListener('click', doAction);
+    }
+}
+activateAnswers();
+
 function clearClass() {
     for (let i = 0; i < answers.length; i++) {
-        answers[i].classList.remove("correct", "incorrect");
+        answers[i].classList.remove('correct', 'incorrect');
     }
 }
-
-function saveResult() {
-
-    if (localStorage.getItem("quizScore") != null) {
-        let poprzWynik = JSON.parse(localStorage.getItem("quizScore"));
-        let nowyWynik = {
-            "avgScore": (poprzWynik.avgScore + points) / (poprzWynik.numberOfGames + 1),
-            "numberOfGames": JSON.parse(localStorage.getItem("quizScore")).numberOfGames + 1
-        };
-        localStorage.setItem("quizScore", JSON.stringify(nowyWynik));
-    } else {
-        let nowyWynik = {
-            "avgScore": points,
-            "numberOfGames": 1
-        };
-        localStorage.setItem("quizScore", JSON.stringify(nowyWynik));
-    }
-    averageScore.innerText = JSON.parse(localStorage.getItem("quizScore")).avgScore;
-
-
-}
-
-
 
